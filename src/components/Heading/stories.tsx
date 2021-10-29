bf1c9f6e7d1a27c1e1466cd8b02320299ae0b1da
Gustavo Seabra <devguga87@gmail.com>
2021-10-29 16:21:18 -0300

    feature: migrando para ts

--- src/components/Heading/stories.tsx
+++ src/components/Heading/stories.tsx
@@ -1,4 +1,5 @@
-import { Heading } from '.';
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { Heading, HeadingProps } from '.';
 
 export default {
   title: 'Heading',
@@ -14,9 +15,9 @@ export default {
       default: 'dark',
     },
   },
-};
+} as Meta;
 
-export const Light = args => <Heading {...args} />;
+export const Light: Story<HeadingProps> = args => <Heading {...args} />;
 export const Dark = args => <Heading {...args} />;
 
 Light.parameters = {
